01cd8d9622eb6986dd2c40d7746b4031c1c3c2c5
Jacob Ives <jacob.w.ives@gmail.com>
2023-04-27 01:25:41 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const element = document.getElementById('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Jacob is the champion'; //I spent half an hour debugging this only to realize I had capitalized Champion so I don't really feel like one right now.
+newHeader.id = "victory";
